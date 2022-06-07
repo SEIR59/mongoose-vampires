12cd6956a6d4598059318312c73b40356134e8a0
Mark Gerken <mgerken86@gmail.com>
2022-06-07 14:26:39 -0600

    dependencies created

---
+++ vampires.js
@@ -0,0 +1,6 @@
+require("dotenv").config(); // Load ENV Variables
+const express = require("express"); // import express
+const morgan = require("morgan"); //import morgan
+const methodOverride = require("method-override");
+const mongoose = require("mongoose");
+const path = require("path")
