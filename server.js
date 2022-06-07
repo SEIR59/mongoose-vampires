a56a4d1f463a61903f7757f980d821e7467d41ee
jhimson <jhimsonrayraboypamisa@gmail.com>
2022-06-07 16:09:02 -0400

    Configured middlewares

--- server.js
+++ server.js
@@ -4,7 +4,21 @@ const morgan = require('morgan'); //import morgan
 const methodOverride = require('method-override');
 const mongoose = require('mongoose');
 const path = require('path');
+const vampire = require('./models/vampire');
 
+//! Create our Express Application Object Bind Liquid Templating Engine
+/////////////////////////////////////////////////
+const app = require('liquid-express-views')(express(), {
+  root: [path.resolve(__dirname, 'views/')],
+});
+
+/////////////////////////////////////////////////////
+//! Middleware
+/////////////////////////////////////////////////////
+app.use(morgan('dev')); //? logging
+app.use(methodOverride('_method')); //? override for put and delete requests from forms
+app.use(express.urlencoded({ extended: true })); //? parse urlencoded request bodies
+app.use(express.static('public')); //? serve files from public statically
 
 /////////////////////////////////////////////
 //! Database Connection
@@ -24,3 +38,5 @@ mongoose.connection
   .on('open', () => console.log('Connected to Mongoose'))
   .on('close', () => console.log('Disconnected from Mongoose'))
   .on('error', (error) => console.log(error));
+
+/////////////////////////////////////////////////
