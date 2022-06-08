e182131bfb3038e1ec8ed098ee35e469d48fd299
NSTaft <taftnathan13@gmail.com>
2022-06-08 15:43:28 -0500

    Created 4 new vampires

--- server.js
+++ server.js
@@ -5,10 +5,69 @@ const mongoose = require('mongoose')
 const port = 3000
 const rowdy = require('rowdy-logger')
 const routesReport = rowdy.begin(app)
+const Vampire = require('./models/vampire.js')
+const db = mongoose.connection
 
 // Homepage
 app.get('/', (req,res) => {
     res.send("Mongoose-Vampires homepagae")
+})
+
+const newVampires = [
+    {
+        name: 'FirstMan',
+        title: 'Count',
+        hair_color: 'brown',
+        eye_color: 'brown',
+        dob: new Date(1971, 2, 13, 7, 47),
+        loves: ['cereal','marshmallows'],
+        location: 'Minneapolis, Minnesota, US',
+        gender: 'm',
+        victims: 2,
+      },
+      {
+        name: 'SecondMan',
+        title: 'Count',
+        hair_color: 'brown',
+        eye_color: 'brown',
+        dob: new Date(1971, 2, 13, 7, 47),
+        loves: ['cereal','marshmallows'],
+        location: 'Minneapolis, Minnesota, US',
+        gender: 'm',
+        victims: 2,
+      },
+      {
+        name: 'FirstWoman',
+        title: 'Count',
+        hair_color: 'brown',
+        eye_color: 'brown',
+        dob: new Date(1971, 2, 13, 7, 47),
+        loves: ['cereal','marshmallows'],
+        location: 'Minneapolis, Minnesota, US',
+        gender: 'f',
+        victims: 2,
+      },
+      {
+        name: 'SecondWoman',
+        title: 'Count',
+        hair_color: 'brown',
+        eye_color: 'brown',
+        dob: new Date(1971, 2, 13, 7, 47),
+        loves: ['cereal','marshmallows'],
+        location: 'Minneapolis, Minnesota, US',
+        gender: 'f',
+        victims: 2,
+      }
+]
+Vampire.insertMany(newVampires)
+.then((newVampires) => {
+    console.log(newVampires)
+})
+.catch((error) => {
+    console.log(error)
+})
+.finally(() => {
+    db.close()
 })
 
 app.listen(3000, () => {
