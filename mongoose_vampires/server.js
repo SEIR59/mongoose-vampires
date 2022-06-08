b830d1862b6237f37c9eb98edeeca82344fd9407
christopher chapa <chapachristopher16@gmail.com>
2022-06-07 20:12:08 -0500

    layout setup needs more done

---
+++ mongoose_vampires/server.js
@@ -0,0 +1,36 @@
+require("dotenv").config(); 
+const express = require("express"); 
+const morgan = require("morgan");
+const methodOverride = require("method-override");
+const mongoose = require("mongoose");
+const path = require("path")
+
+
+
+const DATABASE_URL = process.env.DATABASE_URL;
+const CONFIG = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+mongoose.connect(DATABASE_URL, CONFIG)
+
+mongoose.connection
+  .on("open", () => console.log("Connected to Mongoose"))
+  .on("close", () => console.log("Disconnected from Mongoose"))
+  .on("error", (error) => console.log(error));
+
+  const { Schema, model } = mongoose;
+
+  const vampireSchema = new Schema({
+      name: String,
+      title: String,
+      hair_color: {type: String, default: 'brown'},
+      eye_color: String,
+      dob: Date,
+      loves: Array,
+      location: String,
+      gender: String,
+      victims: {type: Number, default: 1}
+
+  })
