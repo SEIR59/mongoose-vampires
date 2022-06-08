3516c2eb00c3991fc900e8036878e1cc255342e0
earlhalasan <earl.halasan@gmail.com>
2022-06-08 19:58:15 -0400

    Set up Vampire schema and model

---
+++ mongoose_vampires/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const methodOverride = require("method-override");
+const mongoose = require("mongoose");
+
+// Global config
+const mongoURI = "mongodb://127.0.0.1/vampires";
+const db = mongoose.connection;
+
+// Connect to Mongo
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Connection Error/Success
+// Define callback functions for various events
+db.on("error", (err) => console.log(err.message + " is mongod not running?"));
+db.on("open", () => console.log("mongo connected: ", mongoURI));
+db.on("close", () => console.log("mongo disconnected"));
