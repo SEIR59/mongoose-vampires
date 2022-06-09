43fa317678a221a3c652d70e6e5ed50817277b39
mileslathan <mlathan.cn@gmail.com>
2022-06-08 21:53:21 -0400

    ** SETUP COMPLETE ** Added all dependencies and established vampires schema and model

---
+++ vampires.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const model = mongoose.model
+
+
+
+// Building our new Schema that composes a vampire.
+
+const vampireSchema = new Schema({
+    name: String,
+    title: String,
+    hair_color: {
+        type: String,
+        default: 'blonde',
+    },
+    eye_color: String,
+    dob: String,
+    loves: String,
+    location: String,
+    gender: String,
+    victims: {
+        type: Number,
+        default: 2
+    }
+})
+
+// Now building our model from our Schema.
+const Vampire = model("Vampire", vampireSchema)
+
+// Exporting this model of Vampire to be accessed by our app.js
+module.exports = Vampire
