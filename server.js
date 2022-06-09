3fd10034031530e6464e3fde84e017dc8f02b1f7
Edgie Manzo <104868823+emanzo14@users.noreply.github.com>
2022-06-09 09:20:00 -0400

    fixed connection to mongoose, and created vampire schema

--- server.js
+++ server.js
@@ -1,16 +1,25 @@
-const mongoose = require("mongoose");
+require("dotenv").config() // loading env variables
+const express = require("express")
+const morgan = require("morgan")
+const methodOverride = require("method-override")
+const mongoose = require("mongoose")
+const path = require("path")
 
-// Global configuration
-const mongoURI = "mongodb://localhost/vampires";
+// connecting with a database
+const mongoURI = "mongodb://127.0.0.1/vampires";
 const db = mongoose.connection;
-mongoose.connect(mongoURI)
-const express = require('express')
-const app = require('liquid-express-views')(express())
+
+// Connect to Mongo
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// feedback on database connection
+
+db.on("open", () => console.log("Connected to Mongoose"))
+db.on("close", () => console.log("Disconnected from Mongoose"))
+db.on("error", (error) => console.log(error))
 
 
 
 
 
-app.listen(3000,()=>{
-    console.log('now listining on')
-})  
+
