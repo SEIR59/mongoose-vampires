72580ac50e008629274f45de86a0d6bedd950d75
mileslathan <mlathan.cn@gmail.com>
2022-06-12 18:06:48 -0400

    ** Several Values #1 ** Completed.

--- app.js
+++ app.js
@@ -293,8 +293,19 @@ const vampiresDB = [
 //     db.close()
 //   })
 
-Vampire.find({$or: [{hair_color: 'red'}, {eye_color: 'green'}]})
-.then((vampire) => {
+// Vampire.find({$or: [{hair_color: 'red'}, {eye_color: 'green'}]})
+// .then((vampire) => {
+//     console.log(vampire)
+//   })
+//   .catch((error) => {
+//     console.log(error)
+//   })
+//   .finally(() => {
+//     db.close()
+//   })
+
+Vampire.find({$or: [{loves: 'frilly shirtsleeves'}, {loves: 'frilly collars'}]})
+  .then((vampire) => {
     console.log(vampire)
   })
   .catch((error) => {
